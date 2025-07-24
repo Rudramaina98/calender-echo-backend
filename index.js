a9eb631e8172d6575335748f0a9f1746c9b13987
“rudramaina98” <rudramaina.shivani@gmail.com>
2025-07-24 14:06:28 -0400

    running port 8080

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/api/events', eventRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
